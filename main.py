d1d04a5c431bd31fcc1fe73f95c468ace646c088
diako <yazdanghader70@gmail.com>
2022-04-26 14:20:04 +0430

    pull content of all episods tables in (https://en.wikipedia.org/wiki/List_of_Game_of_Thrones_episodes)

--- main.py
+++ main.py
@@ -5,23 +5,22 @@ url ="https://en.wikipedia.org/wiki/List_of_Game_of_Thrones_episodes"
 result = requests.get(url)
 soup = BeautifulSoup(result.text, 'html.parser')
 
-table = soup.find('table',class_="wikitable plainrowheaders wikiepisodetable")
+epTable = soup.findAll('table',class_="wikitable plainrowheaders wikiepisodetable")
 episods = []
-# for table in epTable:
-headers = []
-for header in table.find("tr").findAll("th"):
-    headers.append(header.text)
+for table in epTable:
+    headers = []
+    for header in table.find("tr").findAll("th"):
+        headers.append(header.text)
 
-for row in table.findAll("tr")[1:]:
-    values = []
-    for col in row.findAll(["th","td"]):
-        values.append(col.text)
-    if values:
-        episodDict = {headers[i]:values[i] for i in range(len(values))}
-        episods.append(episodDict)
-print(episods)
-# for ep in episods:
-#     print(ep)
+    for row in table.findAll("tr")[1:]:
+        values = []
+        for col in row.findAll(["th","td"]):
+            values.append(col.text)
+        if values:
+            episodDict = {headers[i]:values[i] for i in range(len(values))}
+            episods.append(episodDict)
+for ep in episods:
+    print(ep)
 
 
 
